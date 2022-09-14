f341710144d8845e968432e0581170e86cec665b
armaan <karmankkhan@gmail.com>
2022-09-14 09:57:53 +0530

    index js done

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import Userrouter from "./Routes/User.js";
+import AuthRouter from "./Routes/Auth.js";
+import HotelRouter from "./Routes/Hotel.js";
+import RoomRouter from "./Routes/Room.js";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+//code for connect to mongo db
+const url =
+  "mongodb+srv://armaankhan:armaan242@cluster0.ygbfntv.mongodb.net/?retryWrites=true&w=majority";
+const connect = () => {
+  try {
+    mongoose.connect(url).then((e) => {
+      console.log("successfully connected to mongodb");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+//code for informing us to we are dissconnected to mongo db
+mongoose.connection.on("disconnect", () => {
+  console.log(" mongodb disconnected");
+});
+//code for informing us to we are conneted to mongo db
+mongoose.connection.on("connected", () => {
+  console.log(" mongodb connected");
+});
+//rotes code here
+app.use("/api/user", Userrouter);
+app.use("/api/auth", AuthRouter);
+app.use("/api/room", RoomRouter);
+app.use("/api/hotel", HotelRouter);
+// app.use("/", (req, res) => res.send("hello Welcome To Booking App"));s
+app.listen(3001, () => {
+  connect();
+  console.log("server in running on port 3001");
+});
